8ea7289eb01b971a3c0fd4f3e4c3a473a62936a5
Theodore <theodoreoovoo@gmail.com>
2021-12-12 11:05:41 +0630

    Add Pizza

--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -14,20 +14,27 @@
                     </ul>
                 </div>
             </div>
+
+            @if(count($errors)>0)
+            <div class="card mt-5">
+                <div class="card-body">
+                    <div>
+                        <div class="alert alert-danger">
+                            @foreach ( $errors->all() as $error)
+                            <p> {{ $error }} </p> 
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endif
         </div>
 
         <!--Pizza-->
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Pizza</div>
-                @if(count($errors)>0)
-                    <div class="alert alert-danger">
-                        @foreach ( $errors->all() as $error)
-                           <p> {{ $error }} </p> 
-                        @endforeach
-                    </div>
-                @endif
-                <form action="{{route('pizza.store')}}" method="post">@csrf
+                <form action="{{route('pizza.store')}}" method="post" enctype="multipart/form-data">@csrf
                     <div class="card-body">
                         <div class="form-control">
                             <label for="name">Name of Pizza</label>
