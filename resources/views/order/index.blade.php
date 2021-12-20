27729da7ce8dbee4c8b77dfcb870214eecd0f9e0
Theodore <theodoreoovoo@gmail.com>
2021-12-20 13:18:50 +0630

    designing order page

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -3,18 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active" aria-current="page">Orders</li>
+                </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Orders</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                   <table class="table table-bordered">
+                       <thead>
+                           <tr>
+                               <th scope="col">User</th>
+                               <th scope="col">Phone/Email</th>
+                               <th scope="col">Date/Time</th>
+                               <th scope="col">Pizza</th>
+                               <th scope="col">Small Pizza</th>
+                               <th scope="col">Medium Pizza</th>
+                               <th scope="col">Large Pizza</th>
+                               <th scope="col">Message</th>
+                               <th scope="col">Status</th>
+                               <th scope="col">Accept</th>
+                               <th scope="col">Reject</th>
+                               <th scope="col">Completed</th>
+                           </tr>
+                       </thead>
+                       <tbody>
+                           <tr>
+                               <th>Mark</th>
+                               <th>Otto</th>
+                               <th>@mdo</th>
+                               <th>Mark</th>
+                               <th>Otto</th>
+                               <th>@mdo</th>
+                               <th>Mark</th>
+                               <th>Otto</th>
+                               <th>@mdo</th>
+                               <th><button class="btn btn-primary">Accept</button></th>
+                               <th><button class="btn btn-primary">Reject</button></th>
+                               <th><button class="btn btn-primary">Completed</button></th>
+                           </tr>
+                       </tbody>
+                   </table>
                 </div>
             </div>
         </div>
